3aa928372e0a2a32bc8ce536190cca228c190f36
Olivier Detour <detour.olivier@gmail.com>
2010-03-25 16:03:20 +0100

    Add good layout in Awesome.

--- rc.lua
+++ rc.lua
@@ -34,11 +34,11 @@ modkey = "Mod1"
 layouts =
 {
     awful.layout.suit.tile,
-    -- awful.layout.suit.tile.left,
+    awful.layout.suit.tile.left,
     awful.layout.suit.tile.bottom,
-    -- awful.layout.suit.tile.top,
-    -- awful.layout.suit.fair,
-    -- awful.layout.suit.fair.horizontal,
+    awful.layout.suit.tile.top,
+    awful.layout.suit.fair,
+    awful.layout.suit.fair.horizontal,
     awful.layout.suit.max,
     -- awful.layout.suit.max.fullscreen,
     -- awful.layout.suit.magnifier,
