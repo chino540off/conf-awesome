187a9db5edba3055b0d77e8bbc2861ab6aab6055
Olivier Detour <detour.olivier@gmail.com>
2016-10-03 09:20:18 +0200

    Remove run_once.

--- rc.lua
+++ rc.lua
@@ -42,20 +42,6 @@ do
 end
 -- }}}
 
--- {{{ Autostart applications
-function run_once(cmd)
-  findme = cmd
-  firstspace = cmd:find(" ")
-  if firstspace then
-     findme = cmd:sub(0, firstspace-1)
-  end
-  awful.util.spawn_with_shell("pgrep -u $USER -x " .. findme .. " > /dev/null || (" .. cmd .. ")")
-end
-
-run_once("urxvtd")
-run_once("unclutter -root")
--- }}}
-
 -- {{{ Variable definitions
 
 -- beautiful init
